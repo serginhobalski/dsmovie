672ac457eb775c6844b73bc6aac82eb73cd7f0fb
Sergio Obalski Filho <serginhobalski@gmail.com>
2022-03-13 11:56:54 -0300

    Services update

--- backend/src/main/java/com/estimulardigital/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/estimulardigital/dsmovie/controllers/ScoreController.java
@@ -1,5 +1,6 @@
 package com.estimulardigital.dsmovie.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.estimulardigital.dsmovie.dto.MovieDTO;
 import com.estimulardigital.dsmovie.dto.ScoreDTO;
+import com.estimulardigital.dsmovie.services.ScoreService;
 
 @RestController
 @RequestMapping(value = "/scores")
 public class ScoreController {
 
+	@Autowired
+	private ScoreService service;
+	
 	@PutMapping
 	public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
-		return null;
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
 	}
 }
--- backend/src/main/java/com/estimulardigital/dsmovie/services/ScoreService.java
+++ backend/src/main/java/com/estimulardigital/dsmovie/services/ScoreService.java
@@ -1,14 +1,17 @@
 package com.estimulardigital.dsmovie.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.estimulardigital.dsmovie.dto.MovieDTO;
+import com.estimulardigital.dsmovie.dto.ScoreDTO;
 import com.estimulardigital.dsmovie.entities.Movie;
+import com.estimulardigital.dsmovie.entities.Score;
+import com.estimulardigital.dsmovie.entities.User;
 import com.estimulardigital.dsmovie.repositories.MovieRepository;
+import com.estimulardigital.dsmovie.repositories.ScoreRepository;
+import com.estimulardigital.dsmovie.repositories.UserRepository;
 
 @Service
 public class ScoreService {
@@ -16,5 +19,46 @@ public class ScoreService {
 	@Autowired
 	private MovieRepository movieRepository;
 	
+	@Autowired
+	private UserRepository userRepository;
 	
+	@Autowired
+	private ScoreRepository scoreRepository;
+	
+	@Transactional
+	public MovieDTO saveScore(ScoreDTO dto) {
+		
+		User user = userRepository.findByEmail(dto.getEmail());
+		if (user == null) {
+			user = new User();
+			user.setEmail(dto.getEmail());
+			user = userRepository.saveAndFlush(user);
+		}
+		
+		Movie movie = movieRepository.findById(dto.getMovieId()).get();
+		
+		Score score = new Score();
+		score.setMovie(movie);
+		score.setUser(user);
+		score.setValue(dto.getScore());
+		
+		score = scoreRepository.saveAndFlush(score);
+		
+		double sum = 0.0;
+		for (Score s : movie.getScores()) {
+			sum = sum + s.getValue();
+		}
+		
+		double avg = sum / movie.getScores().size();
+		
+		movie.setScore(avg);
+		movie.setCount(movie.getScores().size());
+		
+		movie = movieRepository.save(movie); 
+		
+		return new MovieDTO(movie);
+		
+	}
+
 }
+	
